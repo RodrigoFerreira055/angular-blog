b37f7f196d2e5b70330356623af0bc1d0c5c6cc9
Rodrigo <rodrigo.nunes-55@hotmail.com>
2023-07-18 22:35:29 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Lorem Ipsum',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero quidem deserunt cum voluptas vel obcaecati, in alias labore modi distinctiodoloremque, eos similique iste, ullam ipsam aliquam expedita officia vitae.',
+    photoCover:
+      'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg',
+  },
+  {
+    id: '2',
+    title: 'Lorem Ipsum',
+    description: 'Dolor Simetrium',
+    photoCover: '',
+  },
+];
